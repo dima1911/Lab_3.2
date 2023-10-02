09c0a0cc66d0014dc1e38a80ac8c3d432919f79e
Admin <Admin@XIAOMI>
2023-10-02 18:25:41 +0300

    Добавьте файлы проекта.

---
+++ Lab_3.2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class Point
+{
+    public double X { get; }
+    public double Y { get; }
+    public string Name { get; }
+
+    public Point(double x, double y, string name)
+    {
+        X = x;
+        Y = y;
+        Name = name;
+    }
+}
+
+class Figure
+{
+    private Point[] points;
+
+    public Figure(Point point1, Point point2, Point point3)
+    {
+        points = new Point[] { point1, point2, point3 };
+    }
+
+    public Figure(Point point1, Point point2, Point point3, Point point4)
+    {
+        points = new Point[] { point1, point2, point3, point4 };
+    }
+
+    public Figure(Point point1, Point point2, Point point3, Point point4, Point point5)
+    {
+        points = new Point[] { point1, point2, point3, point4, point5 };
+    }
+
+    private double GetSideLength(Point A, Point B)
+    {
+        double sideLength = Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+        return sideLength;
+    }
+
+    public void CalculatePerimeter()
+    {
+        double perimeter = 0;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Point currentPoint = points[i];
+            Point nextPoint = points[(i + 1) % points.Length];
+            perimeter += GetSideLength(currentPoint, nextPoint);
+        }
+        Console.WriteLine($"Назва багатокутника: {points.Length}-кут");
+        Console.WriteLine($"Периметр багатокутника: {perimeter}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Point point1 = new Point(0, 0, "A");
+        Point point2 = new Point(0, 4, "B");
+        Point point3 = new Point(3, 0, "C");
+
+        Figure triangle = new Figure(point1, point2, point3);
+        triangle.CalculatePerimeter();
+    }
+}
